9c160e0c9eb2289177129658583dac481b469352
jjeongdong <donghun3296@naver.com>
2024-05-08 10:52:09 +0900

    feat : Entity 설계 및 매핑

---
+++ src/main/java/com/example/umc/domain/Member.java
@@ -0,0 +1,67 @@
+package com.example.umc.domain;
+
+import com.example.umc.domain.common.BaseEntity;
+import com.example.umc.domain.enums.Gender;
+import com.example.umc.domain.enums.MemberStatus;
+import com.example.umc.domain.enums.SocialType;
+import com.example.umc.domain.mapping.FoodLikes;
+import com.example.umc.domain.mapping.MemberMission;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false, length = 40)
+    private String address;
+
+    @Column(nullable = false, length = 40)
+    private String specAddress;
+
+    @Column(nullable = false, length = 250)
+    private String profileImg;
+
+    private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(10)")
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(15) DEFAULT 'ACTIVE'")
+    private MemberStatus status;
+
+    private LocalDate inactiveDate;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    private Integer point;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<FoodLikes> foodLikesList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Review> reviewList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<MemberMission> memberMissionList = new ArrayList<>();
+}
---
+++ src/main/java/com/example/umc/domain/enums/InquiryStatus.java
@@ -0,0 +1,5 @@
+package com.example.umc.domain.enums;
+
+public enum InquiryStatus {
+    WAITING, COMPLETE
+}
