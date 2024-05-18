bd31b1f9966feec2c6d1943d32f27ed7a8fdad07
jjeongdong <donghun3296@naver.com>
2024-05-18 19:23:23 +0900

    feat : 가게 리뷰 목록 조회 API 구현

--- src/main/java/com/example/umc/service/StoreService/StoreQueryServiceImpl.java
+++ src/main/java/com/example/umc/service/StoreService/StoreQueryServiceImpl.java
@@ -1,8 +1,12 @@
 package com.example.umc.service.StoreService;
 
+import com.example.umc.domain.Review;
 import com.example.umc.domain.Store;
+import com.example.umc.repository.ReviewRepository;
 import com.example.umc.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,8 +19,17 @@ public class StoreQueryServiceImpl implements StoreQueryService{
 
     private final StoreRepository storeRepository;
 
+    private final ReviewRepository reviewRepository;
+
     @Override
     public Optional<Store> findStore(Long id) {
         return storeRepository.findById(id);
+    }
+
+    @Override
+    public Page<Review> getReviewList(Long storeId, Integer page) {
+
+        Store store = storeRepository.findById(storeId).get();
+        return reviewRepository.findAllByStore(store, PageRequest.of(page, 10));
     }
 }
